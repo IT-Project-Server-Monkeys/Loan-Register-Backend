d3b477197ba13874877b038aaf8b090dc979aa39
HuyHLuu <88954722+HuyHLuu@users.noreply.github.com>
2022-08-27 16:37:06 +1000

    Create item get command

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,18 +1,93 @@
+
 const item = require('../models/itemModel')
+var mongoose = require('mongoose');
 
 
-const  getAllItem = async (req,res,next) => {
+const itemHandler = async (req,res,next) => {
+  if (req.query.all && req.query.all.toString() == 1) {
+    getAllItems(req, res, next);
+  }
+  else if (req.query.id) {
+    getSpecificItem(req, res, next);
+  }
+  else if (req.query.category) {
+   
+    getAllItemsbyCategory(req, res, next);
+  }
+
+  else if (req.query.item_owner) {
+    getAllItemByItemOwner(req,res,next);
+    
+  }
+ 
+
+
+}
+
+const  getAllItems = async (req,res,next) => {
   try{
       const result = await item.find().lean()
-      return res.json(result)
+      if ((result.length) > 0) {return res.json(result)}
+      else {res.status(400)}
   } catch (err){
     return next(err)
   }
      
 }
 
-
-
-module.exports = {
-  getAllItem
+const  getSpecificItem = async (req,res,next) => {
+  try{
+    
+    const result = await item.findById(req.query.id).lean()
+    if (!result) {return res.status(400)}
+    console.log(result.loan_frequency)
+    return (res.json(result))
+} catch (err){
+    return next(err)
+  }
 }
+
+const getAllItemsbyCategory = async (req,res,next) => {
+  try{
+    
+    const result = await item.find({category: req.query.category}).lean()
+    if (!result) {return res.status(400)}
+    return res.json(result)
+} catch (err){
+    return next(err)
+  }
+}
+
+const getAllItemByItemOwner = async (req,res,next) => {
+  try{
+    item_owner = new mongoose.Types.ObjectId((req.query.item_owner).toString())
+   
+    const result = await item.find({item_owner: item_owner }).lean()
+    if (!result) {return res.status(400)}
+    return res.json(result)
+} catch (err){
+    return next(err)
+  }
+}
+
+const  getSpecificItemFrequency = async (req,res,next) => {
+  try{
+    
+    const result = await item.findById(req.query.id).lean()
+    if (!result) {return res.status(400)}
+    return res.loan_frequency;
+} catch (err){
+    return next(err)
+  }
+}
+
+
+
+
+
+module.exports= {
+  itemHandler
+}
+
+
+
--- src/routes/itemRouter.js
+++ src/routes/itemRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const itemRouter = express.Router();
 const itemController = require("../controllers/itemController");
-itemRouter.get('/', itemController.getAllItem)
+itemRouter.get('/', itemController.itemHandler);
 
 module.exports = itemRouter
