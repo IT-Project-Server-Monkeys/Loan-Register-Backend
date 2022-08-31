9112622127b88d8ed27a60abdd38e416447791ea
phoebexiong <phoebe@thebearfamily.net>
2022-08-31 12:16:12 +1000

    altered user model to have categories, removed from item

--- src/models/itemModel.js
+++ src/models/itemModel.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
     itemName: { type: String , required: true},
-    category:{ type: String,
-        enum: ["Electronics", "Books", "Stationary", "University Resources", "Cash", "Miscellaneous", "Personal", "Clothing and Apparel", "Toiletries and Beauty"],
-         required: true},
-
+    category:{ type: String, required: true},
     description:{type: String, required: true},
     item_owner: {type: mongoose.Schema.Types.ObjectId, required: true},
     on_loan: {type: Boolean, required: true},
