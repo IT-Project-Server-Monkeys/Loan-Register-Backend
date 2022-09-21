82f807d34f8322bb2d45a329f6c1ecdf15ad0d63
Vivien Guo <guov@student.unimelb.edu.au>
2022-09-21 12:37:04 +1000

    size limit test

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(express.json());
 app.use(
   express.urlencoded({ extended: true })
 );
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
+
 var cors = require("cors");
 app.use(cors({origin: '*'}));
 
