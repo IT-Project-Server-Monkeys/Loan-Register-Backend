c157a76704f06c36a9674315088d87529a1a681b
Vivien Guo <guov@student.unimelb.edu.au>
2022-09-21 15:55:22 +1000

    size testing 2

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const PORT = process.env.PORT || 3000;
 let express = require("express");
 const recordRoutes = express.Router();
 let app = express();
-app.use(express.json());
-app.use(
-  express.urlencoded({ extended: true })
-);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({ 
+  extended: true,
+  limit: '50mb'
+ }));
 
 var cors = require("cors");
 app.use(cors({origin: '*'}));
