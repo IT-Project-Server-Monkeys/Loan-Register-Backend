2a45873ee064e0a287542b6ba7429e0a707c7f49
Vivien Guo <guov@student.unimelb.edu.au>
2022-09-26 21:18:09 +1000

    reboot server

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ 
   extended: true,
   limit: '50mb'
- }));
+ })); 
 
 var cors = require("cors");
 app.use(cors({origin: '*'}));
