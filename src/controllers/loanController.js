7846fe331784147d9fe1ddfe65104e4f46ac6946
Bruce Zhu <76635687+Bruce-zzhu@users.noreply.github.com>
2022-10-10 23:48:01 +1100

    handle null

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -168,8 +168,8 @@ const createLoan = async (req,res,next) => {
     const status = (req.body.status).toString()
     const loanStartDate = new Date(req.body.loan_start_date.toString())
     const intendedReturnDate = new Date(req.body.intended_return_date.toString())
-    const loaneeName = (req.body.loanee_name).toString()
-    const itemImage = (req.body.item_image).toString()
+    const loaneeName = req.body.loanee_name ? (req.body.loanee_name).toString() : ''
+    const itemImage = req.body.item_image ? (req.body.item_image).toString() : ''
     
     const loanResult = await loan.create(
         {loaner_id: loanerId,
